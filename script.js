39a1e5a7e9070761a74c3185e87d581ffd5f92f4
pluizige_gamer <maxbarendregt1@gmail.com>
2026-09-22 16:05:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ mainNav?.querySelectorAll('a').forEach((link) => {
 
 document.querySelector('#year').textContent = new Date().getFullYear();
 
-music.volume = 2;
+music.volume = 1;
 
 musicToggle?.addEventListener('click', async () => {
 	if (music.paused) {
